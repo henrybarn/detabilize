c580ac13948d01edd40f820a21b9aa2d3916d7b0
Henry <me@henrybarnathan.com>
2017-05-20 19:01:40 -0400

    Added comments and two other table attributes

--- jquery/detabilize.jquery.js
+++ jquery/detabilize.jquery.js
@@ -4,23 +4,23 @@
 */
 
 $.fn.divilize = function() {
-    var div = $('<div />');
-    var elemAttrs = this[0].attributes;
-	var tableAttrs = ['cellpadding','cellspacing','border','align','valign','width'];
-    for (var attr = 0; attr < elemAttrs.length; attr++)
-        if (tableAttrs.indexOf(elemAttrs[attr].name.toLowerCase()) === -1)
-            div.attr(elemAttrs[attr].name, elemAttrs[attr].value);
+    var div = $('<div />'); // Creates a div
+    var elemAttrs = this[0].attributes; // Gets attributes of calling element
+	var tableAttrs = ['cellpadding','cellspacing','border','align','valign','width', 'colspan', 'rowspan']; // List of table element attributes
+    for (var attr = 0; attr < elemAttrs.length; attr++) // Loops through attributes of the calling element
+        if (tableAttrs.indexOf(elemAttrs[attr].name.toLowerCase()) === -1) // If attribute is not a table element attribute
+            div.attr(elemAttrs[attr].name, elemAttrs[attr].value); // Assign attribute to new div
         }
-    div.addClass('divilize-'+$(this)[0].tagName.toLowerCase());
-    $(this).contents().unwrap(this).wrapAll(div);
+    div.addClass('divilize-'+$(this)[0].tagName.toLowerCase()); // Assign mandatory class
+    $(this).contents().unwrap(this).wrapAll(div); // Copy children
 }
 
 $.fn.detabilize = function() {
-	var tableElems = ['table','thead','tbody','tfoot','tr','th','td'];
-    if (tableElems.indexOf($(this)[0].tagName.toLowerCase()) !== -1) {
-    	$(this).children().each(function(){
-        	$(this).detabilize();
+	var tableElems = ['table','thead','tbody','tfoot','tr','th','td']; // List of table elements
+    if (tableElems.indexOf($(this)[0].tagName.toLowerCase()) !== -1) { // If calling element is a table element 
+    	$(this).children().each(function(){ // Visits each child of calling element
+        	$(this).detabilize(); // Recursion for children
         });
-    	$(this).divilize();
+    	$(this).divilize(); // Calls function to convert element to div
     }
 }
